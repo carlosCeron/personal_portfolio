eaf07b0dddf2d5dc9ec43fc1f7e827450b5b3463
Carlos Ceron <carlos.ceron.1986@gmail.com>
2021-03-10 23:18:42 -0500

    Trabajando con la entidad blog.

--- personal_portfolio/urls.py
+++ personal_portfolio/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from portfolio import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('blog/', include('blog.urls')),
     path('', views.home),
 ]
 
