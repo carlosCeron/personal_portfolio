5ee513dcb51b08d5d41e249a137c20f8e4fe09b1
Carlos Ceron <carlos.ceron.1986@gmail.com>
2021-03-12 23:42:46 -0500

    Ajustes de templatss.

--- blog/models.py
+++ blog/models.py
@@ -5,4 +5,7 @@ class Blog(models.Model):
 	title = models.CharField(max_length=150)
 	date = models.DateField()
 	description = models.TextField(max_length=300)
+	
+	def __str__(self):
+		return self.title
 
